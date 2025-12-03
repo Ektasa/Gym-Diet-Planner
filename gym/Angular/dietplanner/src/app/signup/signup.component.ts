5440c3a50f72f7e2dd85c736c74ded58c23cc442
Ektasa <adleyadsion2r@gmail.com>
2025-12-04 00:26:09 +0530

    sign up page

--- gym/Angular/dietplanner/src/app/signup/signup.component.ts
+++ gym/Angular/dietplanner/src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -6,9 +7,36 @@ selector: 'app-signup',
 templateUrl: './signup.component.html',
 styleUrls: ['./signup.component.css']
 })
+// export class SignupComponent {
+// username = '';
+// email = '';
+// password = '';
+// }
+
 export class SignupComponent {
-username = '';
-email = '';
-password = '';
+  signupForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  signup() {
+    const data = this.signupForm.value;
+    // call backend API
+    fetch('http://localhost:8080/api/auth/signup', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
+    }).then(r => r.text()).then(t => alert(t)).catch(e => console.error(e));
+  }
+
+  continueWithGoogle() {
+    window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+  }
+
+  continueWithOutlook() {
+    window.location.href = 'http://localhost:8080/oauth2/authorization/microsoft';
+  }
 }
 
