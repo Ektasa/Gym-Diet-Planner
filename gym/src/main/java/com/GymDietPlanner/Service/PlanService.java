d43e42ccc3af3947bfa4926aa10dd7d2cea5d9ca
Ektasa <adleyadsion2r@gmail.com>
2025-12-29 23:30:11 +0530

    fix

--- gym/src/main/java/com/GymDietPlanner/Service/PlanService.java
+++ gym/src/main/java/com/GymDietPlanner/Service/PlanService.java
@@ -2,6 +2,8 @@ package com.GymDietPlanner.Service;
 
 
 import com.GymDietPlanner.Entity.Plan;
+import com.GymDietPlanner.Entity.PlanDuration;
+import com.GymDietPlanner.Repository.PlanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
